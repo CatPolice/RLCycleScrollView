13c4558889e08d34e88c0f974ccc862c4b354a68
gavin <215742216@qq.com>
2017-03-17 14:47:27 +0800

    IBOutlet

--- RLCycleScrollView/RLCycleScrollView.swift
+++ RLCycleScrollView/RLCycleScrollView.swift
@@ -38,7 +38,12 @@ class RLCycleScrollView: UIView , UIScrollViewDelegate{
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+//        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
 
 }
--- RLCycleScrollView/ViewController.swift
+++ RLCycleScrollView/ViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+@IBOutlet weak var cycleView: RLCycleScrollView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let imgList:NSMutableArray = ["0.jpg","1.jpg","2.jpg"]
-        
         let width = UIScreen.main.bounds.size.width
         
-        let view_ = RLCycleScrollView.init(frame: CGRect(x:0,y:100,width:width,height:width*(9/16)))
+        
+        let view_ = RLCycleScrollView.init(frame: CGRect(x:0,y:200,width:width,height:width*(9/16)))
         view_.setItems(itemsPar: imgList)
         view_.downloadItem { (imgview, index) in
             print("======== download img ========")
@@ -35,7 +39,29 @@ class ViewController: UIViewController {
         }
         
         self.view.addSubview(view_)
+        
+        
+        
+        self.cycleView.setItems(itemsPar: imgList)
+        self.cycleView.downloadItem { (imgView, index) in
+            print("======== download img ========")
+            print(imgView)
+            print(index)
+            
+            imgView.image = UIImage.init(named: imgList[index] as! String)
+            
+            //网络图片也在此处下载显示
+        }
+        
+        
+        self.cycleView.didSelectItem { (index) in
+            print(index)
+        }
+        
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
